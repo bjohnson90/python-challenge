6a559a342d029d4f885c6f681300630943cc783d
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-03-26 21:05:13 -0500

    forgot to capture the month

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,9 @@ with open("./Resources/budget_data.csv", newline="") as csvfile:
         totMonths = 0
         netPL = 0
         greatestProfit = 0
+        greatestProfitMonth = ""
         greatestLoss = 0
+        greatestLossMonth = ""
         next(rows)
         for row in rows:
             totMonths+=1
@@ -15,15 +17,17 @@ with open("./Resources/budget_data.csv", newline="") as csvfile:
             netPL+= profitLoss
             if greatestProfit < profitLoss:
                 greatestProfit = profitLoss
+                greatestProfitMonth = row[0]
             if greatestLoss > profitLoss:
                 greatestLoss = profitLoss
+                greatestLossMonth = row[0]
         
         resultfile.write("Financial Analysis\n----------------------------\n")
         resultfile.write(f"Total Months: {totMonths}\n")
         resultfile.write(f"Total: ${netPL}\n")
         resultfile.write("Average Change: ${:.2f}\n".format(float(netPL)/totMonths))
-        resultfile.write(f"Greatest Increase in Profits: ${greatestProfit}\n")
-        resultfile.write(f"Greatest Decrease in Profits: ${greatestLoss}\n")
+        resultfile.write(f"Greatest Increase in Profits: {greatestProfitMonth} ${greatestProfit}\n")
+        resultfile.write(f"Greatest Decrease in Profits: {greatestLossMonth} ${greatestLoss}\n")
     print("\nFinancial Analysis\n----------------------------")
     print(f"Total Months: {totMonths}")
     print(f"Total: ${netPL}")
