b63097b198ea4ae6c28e23102dfe8e979d23423e
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-03-26 20:54:12 -0500

    changed formatting style to new formatting style

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,6 +27,6 @@ with open("./Resources/budget_data.csv", newline="") as csvfile:
     print("\nFinancial Analysis\n----------------------------")
     print(f"Total Months: {totMonths}")
     print(f"Total: ${netPL}")
-    print("Average Change: $%.2f" % (float(netPL)/totMonths))
+    print("Average Change: ${:.2f}\n".format(float(netPL)/totMonths))
     print(f"Greatest Increase in Profits: ${greatestProfit}")
     print(f"Greatest Decrease in Profits: ${greatestLoss}")
