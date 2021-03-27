2b07cf8c231e61af68b2b9c9017cc8941bcac2bb
Brittany Johnson <brittanyjohnson16@yahoo.com>
2021-03-26 19:50:38 -0500

    script writes results to file as well

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,21 +2,28 @@ import csv
 
 
 with open("./Resources/budget_data.csv", newline="") as csvfile:
-    rows = csv.reader(csvfile)
-    totMonths = 0
-    netPL = 0
-    greatestProfit = 0
-    greatestLoss = 0
-    next(rows)
-    for row in rows:
-        totMonths+=1
-        profitLoss = int(row[1])
-        netPL+= profitLoss
-        if greatestProfit < profitLoss:
-            greatestProfit = profitLoss
-        if greatestLoss > profitLoss:
-            greatestLoss = profitLoss
-    
+    with open("./Analysis/results.txt", 'w') as resultfile:
+        rows = csv.reader(csvfile)
+        totMonths = 0
+        netPL = 0
+        greatestProfit = 0
+        greatestLoss = 0
+        next(rows)
+        for row in rows:
+            totMonths+=1
+            profitLoss = int(row[1])
+            netPL+= profitLoss
+            if greatestProfit < profitLoss:
+                greatestProfit = profitLoss
+            if greatestLoss > profitLoss:
+                greatestLoss = profitLoss
+        
+        resultfile.write("Financial Analysis\n----------------------------\n")
+        resultfile.write(f"Total Months: {totMonths}\n")
+        resultfile.write(f"Total: ${netPL}\n")
+        resultfile.write("Average Change: $%.2f\n" % (float(netPL)/totMonths))
+        resultfile.write(f"Greatest Increase in Profits: ${greatestProfit}\n")
+        resultfile.write(f"Greatest Decrease in Profits: ${greatestLoss}\n")
     print("\nFinancial Analysis\n----------------------------")
     print(f"Total Months: {totMonths}")
     print(f"Total: ${netPL}")
